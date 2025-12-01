c345590c248d61edb517fcacdbc92be3e3f0c84f
CamillaEva <caha0007@stud.kea.dk>
2025-12-01 11:16:23 +0100

    healthcontroller added

---
+++ src/main/java/org/example/backend/controller/HealthController.java
@@ -0,0 +1,16 @@
+package org.example.backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/healthz")
+    public Map<String, String> healthz() {
+        return Map.of("status", "UP");
+    }
+}
