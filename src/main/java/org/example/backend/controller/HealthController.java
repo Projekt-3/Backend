725e94eabe82ef4435470369fd5611b9fa9fc586
CamillaEva <caha0007@stud.kea.dk>
2025-12-01 11:50:36 +0100

    edit branch tag

--- src/main/java/org/example/backend/controller/HealthController.java
+++ src/main/java/org/example/backend/controller/HealthController.java
@@ -2,6 +2,7 @@ package org.example.backend.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -10,6 +11,7 @@ import java.util.Map;
 public class HealthController {
 
     @GetMapping("/healthz")
+    @ResponseBody
     public Map<String, String> health() {
         return Map.of("status", "UP");
     }
