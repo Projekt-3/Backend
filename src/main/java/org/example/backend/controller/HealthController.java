2800a76ce1578eee1ac6def706957053ed9719f6
CamillaEva <caha0007@stud.kea.dk>
2025-12-01 11:19:30 +0100

    small edit in health controller

--- src/main/java/org/example/backend/controller/HealthController.java
+++ src/main/java/org/example/backend/controller/HealthController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class HealthController {
 
     @GetMapping("/healthz")
-    public Map<String, String> healthz() {
+    public Map<String, String> health() {
         return Map.of("status", "UP");
     }
 }
