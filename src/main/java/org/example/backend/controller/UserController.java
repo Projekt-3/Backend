9f1abc7ee186feb4365cfa4c522ab18808812d7c
KasperPersson <kape0010@stud.kea.dk>
2025-12-04 14:46:45 +0100

    PROJ-44 added global exception handler, added password and email validators and added EmployeeService class and  refactored UserController

--- src/main/java/org/example/backend/controller/UserController.java
+++ src/main/java/org/example/backend/controller/UserController.java
@@ -2,6 +2,7 @@ package org.example.backend.controller;
 
 import org.example.backend.model.Employee;
 import org.example.backend.repository.IEmployeeRepository;
+import org.example.backend.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,34 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/dashboard")
+@RequestMapping("/dashboard")
 public class UserController {
 
     @Autowired
-    IEmployeeRepository iEmployeeRepository;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
+    EmployeeService employeeService;
 
 
     // Til at registrere nye users
     @PostMapping("/manager/register")
-    public ResponseEntity<String> registerUser(@RequestBody Employee employee) {
-        Employee savedEmployee = null;
-        ResponseEntity response = null;
-        try {
-            String hashPwd = passwordEncoder.encode(employee.getPassword());
-            employee.setPassword(hashPwd);
-            savedEmployee = iEmployeeRepository.save(employee);
-            if (savedEmployee.getId() > 0) {
-                response = ResponseEntity.status(HttpStatus.CREATED)
-                        .body("Given user details are successfully registrered");
-            }
-        } catch (Exception ex) {
-            response = ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("An exception due to" + ex.getMessage());
-        }
-        return response;
+    public ResponseEntity<String> createUser(@RequestBody Employee employee) {
+        Employee savedEmployee = employeeService.createEmployee(employee);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Brugeren er oprettet med id: " + savedEmployee.getId());
     }
 }
---
+++ src/main/java/org/example/backend/service/EmployeeService.java
@@ -0,0 +1,44 @@
+package org.example.backend.service;
+
+import org.example.backend.model.Employee;
+import org.example.backend.repository.IEmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    IEmployeeRepository iEmployeeRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Employee createEmployee(Employee employee) {
+        validatePassword(employee.getPassword());
+        validateEmailUnique(employee.getMail());
+
+        employee.setPassword(passwordEncoder.encode(employee.getPassword()));
+
+        return iEmployeeRepository.save(employee);
+    }
+
+    private void validatePassword (String password) {
+        if (password.length() < 8) {
+            throw new IllegalArgumentException ("Password skal indeholde mindst 8 tegn");
+        }
+        if (!password.matches(".*[A-Z].*")) {
+            throw new IllegalArgumentException("Password skal indeholde mindst ét stort bogstav");
+        }
+        if (!password.matches(".*\\d.*")) {
+            throw new IllegalArgumentException("Password skal indeholde mindst ét tal");
+        }
+    }
+
+    private void validateEmailUnique (String mail) {
+        if (iEmployeeRepository.findByMail(mail).isPresent()) {
+            throw new IllegalArgumentException("Email is already registered");
+        }
+    }
+}
