435f2fa2181e481c8ccea1252937e8b49575198d
laerkeloenborg <lalo0001@stud.kea.dk>
2025-12-17 10:30:43 +0100

    commit

--- src/main/java/org/example/backend/controller/ShiftController.java
+++ src/main/java/org/example/backend/controller/ShiftController.java
@@ -5,7 +5,6 @@ import org.example.backend.model.Shift;
 import org.example.backend.service.ShiftService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
